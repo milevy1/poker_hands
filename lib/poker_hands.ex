a2dc54b077e6b55e816d21186cf57d1a4d194628
Matt Levy <milevy1@gmail.com>
2019-10-03 16:41:22 -0500

    Adds .winner? function to assess one line with two hands - Includes ranking function to rank a royal_flush hand, all other hands will drop to high card currently - Includes pseudocode for other rankings of cards - Includes .values and .suites helper functions to break the cards up into a list of values and suites - .royal_flush function which checks if the hand is a royal_flush - .flush function which checks if the hand is a flush

--- lib/poker_hands.ex
+++ lib/poker_hands.ex
@@ -1,18 +1,59 @@
 defmodule PokerHands do
-  @moduledoc """
-  Documentation for PokerHands.
-  """
+  def winner?(cards_string) do
+    p1_cards = String.split(cards_string, " ") |> Enum.take(5)
+    p2_cards = String.split(cards_string, " ") |> Enum.take(-5)
 
-  @doc """
-  Hello world.
+    p1_ranking = ranking(p1_cards)
+    p2_ranking = ranking(p2_cards)
 
-  ## Examples
+    cond do
+      p1_ranking < p2_ranking -> :p1
+      p1_ranking > p2_ranking -> :p2
+    end
+  end
 
-      iex> PokerHands.hello()
-      :world
+  def ranking(cards) do
+    values = values(cards)
+    suites = suites(cards)
 
-  """
-  def hello do
-    :world
+    cond do
+      royal_flush?(values, suites) -> 1
+      # straight_flush?(values, suites) -> 2
+      # four_of_a_kind?(values) -> 3
+      # full_house?(values) -> 4
+      flush?(suites) -> 5
+      # straight?(values) -> 6
+      # three_of_a_kind?(values) -> 7
+      # two_pairs -> 8
+      # one_pair -> 9
+      true -> 10 # high card
+    end
+  end
+
+  def values(cards) do
+    Enum.map(cards, &(String.first/1))
+    |> Enum.sort
+  end
+
+  def suites(cards) do
+    Enum.map(cards, &(String.last/1))
+  end
+
+  def royal_flush?(["A", "J", "K", "Q", "T"], suites) do
+    cond do
+      flush?(suites) -> true
+      true -> false
+    end
+  end
+  def royal_flush?(_values, _suites), do: false
+
+  def flush?(suites) do
+    case suites do
+      ["C", "C", "C", "C", "C"] -> true
+      ["D", "D", "D", "D", "D"] -> true
+      ["S", "S", "S", "S", "S"] -> true
+      ["H", "H", "H", "H", "H"] -> true
+      _ -> false
+    end
   end
 end
--- test/poker_hands_test.exs
+++ test/poker_hands_test.exs
@@ -1,8 +1,9 @@
 defmodule PokerHandsTest do
   use ExUnit.Case
-  doctest PokerHands
 
-  test "greets the world" do
-    assert PokerHands.hello() == :world
+  # @tag :skip
+  test "Royal flush beats all other hands" do
+    assert PokerHands.winner?("TC JC QC KC AC 7D 2S 5D 3S AC") == :p1
+    assert PokerHands.winner?("7D 2S 5D 3S AC TC JC QC KC AC") == :p2
   end
 end
